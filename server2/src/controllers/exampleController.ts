dccf77c197c4a0e051ba82046609d166551bbf14
Hugo Hoffmann <hugohoffmann04@gmail.com>
2021-01-21 22:07:12 -0300

    chore: update controller server2

--- server2/src/controllers/exampleController.ts
+++ server2/src/controllers/exampleController.ts
@@ -1,14 +1,21 @@
-import { Request, Response } from "express"
-import { ExampleService } from '../services/exampleService'
+import { Request, Response } from "express";
+import { RabbitmqService } from "../services/rabbitmqService";
+import { ExampleService } from "../services/exampleService";
 
-export class ExampleController{
-  public exampleService: ExampleService
+export class ExampleController {
+  public exampleService: ExampleService;
 
   constructor() {
-    this.exampleService = new ExampleService()
+    this.exampleService = new ExampleService();
   }
 
-  public dispatchPub(req: Request, res: Response){
-    res.status(200).json({ok: true})
+  public async dispatchPub(req: Request, res: Response) {
+    const server = new RabbitmqService("amqp://admin:admin@rabbitmq:5672");
+    await server.start();
+    await server.consume("server2", (message) =>
+      console.log(message.content.toString())
+    );
+
+    res.status(200).json({ ok: true });
   }
 }
